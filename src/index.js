6a9c299e07d5931208c8c956c52700ee132fee6e
Aaron Melendez <aaronmelendez37@gmail.com>
2024-02-07 18:23:05 -0800

    run tcpdump command with spawn

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 // using child-process module spawn function to spin up process to execute tcpdump command
-// need the child process to be on going
+// process can run `sudo tcpdump -i en0 'tcp or udp'`
+// see if upload/download can be displayed in real time?
+// parse out lengh value from command output and add up.
+// upload data = when source ip is my machine
+// download data = when source ip is from ip other than mine
+// display
+
+const { spawn } = require("child_process");
+// no longer throwing error, but no output
+const child = spawn("sudo", ["tcpdump", "-i", "en0", "tcp", "or", "udp"]);
+
+child.stdout.on("data", (data) => {
+  console.log(data);
+});
