84199de582c064a407dde67278d804250f4e002a
Aaron Melendez <aaronmelendez37@gmail.com>
2024-02-11 11:34:30 -0800

    convert tcpdump binary output to string

--- src/index.js
+++ src/index.js
@@ -5,11 +5,19 @@
 // upload data = when source ip is my machine
 // download data = when source ip is from ip other than mine
 // display
+// sudo visudo, remove last line
 
 const { spawn } = require("child_process");
 // no longer throwing error, but no output
 const child = spawn("sudo", ["tcpdump", "-i", "en0", "tcp", "or", "udp"]);
+let upload = 0;
+let download = 0;
 
 child.stdout.on("data", (data) => {
-  console.log(data);
+  console.log(data.toString());
+  const tcpdumpOutput = data.toString();
+  upload += getUploadLength(tcpdumpOutput);
+  download += getDownloadLength(tcpdumpOutput);
 });
+
+// on exit of process show totalled upload and download
